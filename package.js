a11d320dd268a42e8101cb9f10456fae4c3fb57b
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-04-21 15:31:07 +0300

    Update package.js

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   summary: "Method to add first Meteor user to Admin Role.",
-  version: "0.1.1",
-  name: "brylie:first-admin",
-  git: "https://github.com/brylie/meteor-first-user-admin"
+  version: "0.1.2",
+  name: "apinf:first-admin",
+  git: "https://github.com/apinf/meteor-first-user-admin"
 });
 
-Package.on_use(function (api, where) {
-  api.versionsFrom("1.0.1");
-  api.use("accounts-base", "server");
-  api.use("alanning:roles@1.2.11", "server");
-  api.use("brylie:create-role-if-undefined@0.1.0", "server");
+Package.onUse(function (api) {
+  api.versionsFrom('1.2.1');
 
-  api.add_files('server/methods/users.js', "server");
-	api.add_files('server/accounts.js', "server");
+  api.use("accounts-base", ["server"]);
+  api.use("apinf:create-role-if-undefined@0.1.1", ["server"]);
+
+  api.addFiles('server/methods/users.js', ["server"]);
+	api.addFiles('server/accounts.js', ["server"]);
 });
